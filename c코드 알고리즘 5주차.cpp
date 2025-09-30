4035861c53424ec1ce9e99462725e47b0bd4d9b7
sssramail <zion1nice@gmail.com>
2025-09-30 18:50:42 +0900

    Add files via upload

---
+++ c코드 알고리즘 5주차.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+/*-+	int n,m; //Â¦±Ã ¹®Á¦ 
+	vector<int> v [100001];
+	
+	scanf("%d %d",&n,&m);
+	
+	for (int i = 0;i < m;i++)
+	{
+		int from,to;
+		scanf("%d %d",&from,&to);
+		v[from].push_back(to);
+	}
+	for (int i = 1;i <= n;i++)
+	{
+		for (int j = 0;j < v[i].size();j++)
+		{
+			printf("%d ",v[i][j]);
+		}
+		if (v[i].size() == 0)
+		{
+			printf("0");
+		}
+		printf("\n");
+	}
+	
+	int a[100][100];
+	int n;
+	int m[10][10];
+	
+	scanf("%d",&n);
+	
+	for (int i = 0;i < n;i++)
+	{
+		scanf("%d %d",&m[i][i]);
+	}*/
+	
+	int map[101][101];
+	int n;
+	int torwhddl[101][101];
+	
+	scanf("%d",&n);
+	
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = 0;j = 0;j++)
+		{
+			scanf("%d %d %d %d",&torwhddl[i][j],&torwhddl[i][j],&torwhddl[i][j],&torwhddl[i][j]); 
+		}
+	}
+	
+	return 0;	
+}
