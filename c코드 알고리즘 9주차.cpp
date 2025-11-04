f04c2b6ae2086b8d469c6ce390ad219cf28a7f18
sssramail <zion1nice@gmail.com>
2025-11-04 18:51:06 +0900

    Add files via upload

---
+++ c코드 알고리즘 9주차.cpp
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+/*int chek(int st,int en)
+{
+	if (st >= en)
+	{
+		return 1;
+	}
+	else if (st == en)
+	{
+		return chek(st + 1,en - 1);
+	}
+	else 
+	{
+		return 0;
+	}
+}*/
+
+int main()
+{
+/*	char str[1000];
+	int count[26] = {0,};
+	int result = 0,index;
+	
+	scanf("%s",str);
+	
+	for (int i = 0;str[i];i++)
+	{
+		if('a' <= str[i] && str[i] <= 'z')
+		{
+			str[i] = str[i] - 'A' + 'a';
+		}
+		count[str[i] - 'A']++;
+	}
+	for (int i = 0;i < 26;i++)
+	{
+		if (count[i] > )
+		{
+			count[i];
+			index = i;
+			result = 0;
+			printf(" %d ",count[i]);
+		}
+	}*/
+	
+/*	char str[1000];
+	int index[26] = {0, };
+	
+	
+	scanf("%s",str);
+	
+	for (int i = 0;i < 26;i++)
+	{
+		index[i] = str[i];
+	}
+	for (int i = 0;i < 26;i++)
+	{
+		if (index >= 0)
+		{
+			printf(" %d ",index[i]);
+		}
+		else
+		{
+			printf(" -1 ");
+		}
+	}*/
+	
+/*	char n[1000000];
+	int count = 0;
+	int i = 0;
+	
+	scanf("%[^\n]s",&n);
+	
+	while (1)
+	{
+		i++;
+		
+		if(n[i] == 'NULL')
+		{
+			count += 1;
+		}
+		if (i)
+	}
+	
+	printf("%d",count);*/
+	
+/*	int n;
+	char s[100];
+	int ok;
+	
+	scanf("%d",&n);
+	
+	for (int i = 0;i < n;i++)
+	{
+		scanf("%s",&s[i]);
+		
+		ok = chek(s[i],s[i+1]);
+	}*/
+	
+/*	int n[8];
+	int count[10] = {0, };
+	
+	for (int i = 0;i < 3;i++)
+	{
+		scanf("%d",&n[i]);
+		
+		count[i] = n[i];
+	}
+	
+	printf("%d",count);*/
+	
+	return 0;
+}
