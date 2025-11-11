5f58a6ec18dfa110ce0e28d460de2a9185d5874f
sssramail <zion1nice@gmail.com>
2025-11-11 18:49:46 +0900

    Add files via upload

---
+++ c코드 알고리즘 10주차.cpp
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <utility>
+using namespace std;
+
+/*struct sit
+{
+	int a1,a2;
+	int b1,b2;
+}*/
+
+int main()
+{
+/*	int n,m;
+	
+	scanf("%d",&n);
+	
+	int id[n],ok[n],no[n];
+	char name[n],chekname[n];
+	
+	for (int i = 0;i < n;i++)
+	{
+		scanf("%d",&id[i]);
+		scanf("%s",&name[i]);
+		scanf("%d",&ok[i]);
+		scanf("%d",&no[i]);
+	} 
+	
+	scanf("%d",&m);
+	
+	for (int i = 0;i < m;i++)
+	{
+		scanf("%s",&chekname[i]);
+	}*/
+	
+/*	int n;
+	int max = 0;
+	
+	scanf("%d",&n);
+	
+	int t[n],a[n];
+	
+	for (int i = 0;i < n;i++)
+	{
+		scanf("%d",&t[i]);
+		scanf("%d",&a[i]);
+	}
+	for (int i = 0;i < n;i++)
+	{
+		if (t[i] > max)
+		{
+			max = i;
+		}
+		else if (t[i] == max)
+		{
+			if (a[i] > a[max])
+			{
+				max = i;
+			}
+			else
+			{
+				max = i;
+			}
+		}
+	}
+	
+	printf("%d",max);*/	
+	
+/*	int n[100],m[100];
+	int s,h;
+	
+	scanf("%d %d",&n,&m);
+	
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = 0;j < m;j++)
+		{
+			printf("%d",j);
+			scanf("%d",&m[j]);
+		}
+		scanf("\n");
+	}
+	
+	scanf("%d %d",&sit.a1,&sit.a2);
+	scanf("%d %d",&sit.b1,&sit.b2);*/
+	
+/*	int n;
+	pair <int,int> mt;
+	int max = 0;
+	
+	scanf("%d",&n);
+	
+	for (int i = 0;i < n;i++)
+	{
+		scanf("%d %d",&mt.first,&mt.second);
+	}
+	for (int i = 0;i < n;i++)
+	{
+		if (mt.first + mt.second > max)
+		{
+			max = mt.first;
+		}
+	}*/
+	
+	return 0;
+}
