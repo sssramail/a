59da07148fbfa486bf77f62954a649d6f20ab9b4
sssramail <zion1nice@gmail.com>
2025-11-27 16:53:13 +0900

    Add files via upload

---
+++ c코드 알고리즘 숙제(n번째 큰수,신입사원).cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <utility>
+#include <stack>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+/*	int n;
+	int a[10];
+	int m;
+	
+	scanf("%d",&n);
+	
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = 0;j < 10;j++)
+		{
+			scanf("%d",&a[j]);
+		}
+		sort(a,a+10);
+		m = a[7];
+		printf("%d\n",m);
+	}*/
+	
+	int t;
+	int n;
+	int a,b;
+	int h = 0;
+	
+	scanf("%d",&t);
+	
+	scanf("%d",&n);
+	
+	for (int j = 0;j < t;j++)
+	{
+		for(int i = 0;i < n;i++)
+		{
+			scanf("%d %d",&a,&b);
+		}
+	}
+	
+	
+	
+	return 0;
+}
